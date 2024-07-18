560424d20960b656742013ca2b74e167b78fa975
Mahesh M <mnairmahesh26@gmail.com>
2024-07-18 22:21:00 +0530

    added js code and made some code updates

---
+++ script.js
@@ -0,0 +1,9 @@
+function main(){
+    let htmlCode = document.getElementById("html_area");
+    let cssCode = document.getElementById("css_area");
+    let jsCode = document.getElementById("js_area");
+
+    let output = document.getElementById("result_area");
+
+    output.contentDocument.body.innerHTML = htmlCode.value +"<style>"+ cssCode.value +"<style>"+ jsCode.value;
+}
